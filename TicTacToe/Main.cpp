a13d78030957fb6e9189b1d88955e770461cfe6a
Keloran <keloran@develbox.info>
2014-12-02 14:12:40 +0000

    Changes woot

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -1,12 +1,14 @@
 #include <Main.hpp>
-#include <Valkyrie/Player.hpp>
+#include <Valkyrie/Player/Player.hpp>
 
 namespace NordicArts {
     namespace Game {
         int Main() {
             // Player
-            Valkyrie::Player oPlayer;
+            Valkyrie::Player oPlayer(12);
             Valkyrie::Player *pPlayer = &oPlayer;
+
+            printIt(pPlayer->getLife());
         
             return EXIT_SUCCESS;
         }
